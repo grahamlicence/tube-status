2025d6cd1736e44246ecc9bc8d2995b16dc26e8f
Graham Licence <git@grahamlicence.co.uk>
2016-01-19 08:48:22 +0000

    Fix for icon title text

--- extension/background.js
+++ extension/background.js
@@ -19762,22 +19762,22 @@ function updateIcon() {
     // set icon
     if (TubeStore.plannedClosure() > 0) {
         chrome.browserAction.setIcon({ path: 'images/bad.png' }); //red
-        chrome.browserAction.setTitle({ title: TubeStore.plannedClosureLine() + ' closed' });
+        chrome.browserAction.setTitle({ title: TubeStore.plannedClosureLine() });
     } else if (TubeStore.suspended() > 0) {
         chrome.browserAction.setIcon({ path: 'images/bad.png' }); //red
-        chrome.browserAction.setTitle({ title: TubeStore.suspendedLine() + ' suspended' });
+        chrome.browserAction.setTitle({ title: TubeStore.suspendedLine() });
     } else if (TubeStore.partSuspended() > 0) {
         chrome.browserAction.setIcon({ path: 'images/bad.png' }); //red
-        chrome.browserAction.setTitle({ title: TubeStore.partSuspendedLine() + ' part suspended' });
+        chrome.browserAction.setTitle({ title: TubeStore.partSuspendedLine() });
     } else if (TubeStore.severeDelays() > 0) {
         chrome.browserAction.setIcon({ path: 'images/bad.png' }); //amber
-        chrome.browserAction.setTitle({ title: TubeStore.severeDelaysLine() + ' severe delays' });
+        chrome.browserAction.setTitle({ title: TubeStore.severeDelaysLine() });
     } else if (TubeStore.specialService() > 0) {
         chrome.browserAction.setIcon({ path: 'images/delay.png' }); //alert
-        chrome.browserAction.setTitle({ title: TubeStore.specialServiceLine() + ' special service' });
+        chrome.browserAction.setTitle({ title: TubeStore.specialServiceLine() });
     } else if (TubeStore.minorDelays() > 0) {
         chrome.browserAction.setIcon({ path: 'images/delay.png' }); //yellow
-        chrome.browserAction.setTitle({ title: TubeStore.minorDelaysLine() + ' minor delays' });
+        chrome.browserAction.setTitle({ title: TubeStore.minorDelaysLine() });
     } else {
         chrome.browserAction.setIcon({ path: 'images/good.png' });
         chrome.browserAction.setTitle({ title: 'No issues reported' });
